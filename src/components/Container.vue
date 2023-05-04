758fc6f714ceee26e6ce7c2535f1023b40cac781
haseuk <73511954+haseuk@users.noreply.github.com>
2023-05-04 20:49:11 +0900

    vuestagram

---
+++ src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Post v-for="post in postsData" :key="post" :post="post"/>
+  </div>
+</template>
+
+<script>
+import Post from "@/components/Post";
+
+export default {
+  name: "Container",
+  components: { Post },
+  props: {
+    postsData: Array,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
